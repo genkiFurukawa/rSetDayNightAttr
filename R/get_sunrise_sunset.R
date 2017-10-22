7a37ed934ce35cb073c9637c587eb56938d60e42
Genki Furukawa <genki.furukawa@gmail.com>
2017-10-22 22:54:47 +0900

    calcsunriseを使って書き直し

--- R/get_sunrise_sunset.R
+++ R/get_sunrise_sunset.R
@@ -5,31 +5,22 @@
 #' @param day 日
 #' @param lat 緯度
 #' @param lng 経度
+#' @param tz タイムゾーン
 #' @return 日の出の時間と日の入りの時間の数値のリスト
 #' @examples
-#' get_sunrise_sunset(2017, 1, 13, 45.1 ,135.4)
-#' get_sunrise_sunset(2010, 3, 31, 25.1 ,138.4)
-get_sunrise_sunset <- function(year, month, day, lat, lng){
-  # 引数の数足りているか
-  if(missing(year) || missing(month) || missing(day) || missing(lat) || missing(lng)){
-    stop("arguments is missing!!")
-  }
-  # 引数が数値か
-  if(!is.numeric(year) || !is.numeric(month) || !is.numeric(day) || !is.numeric(lat) || !is.numeric(lng) ){
-    stop("arguments is not numeric!!")
-  }
-  url_name <- "http://labs.bitmeister.jp/ohakon/api/?mode=sun_moon_rise_set"
-  url_name <- paste(url_name, "&year=", year, sep="")
-  url_name <- paste(url_name, "&month=", month, sep="")
-  url_name <- paste(url_name, "&day=", day, sep="")
-  url_name <- paste(url_name, "&lat=", lat, sep="")
-  url_name <- paste(url_name, "&lng=", lng, sep="")
-  res <- xml2::read_xml(url_name)
-  # 結果のチェックいるよね
-  sunrise_time <- as.numeric(xml_text(xml_find_all(res, ".//sunrise")))
-  sunset_time <- as.numeric(xml_text(xml_find_all(res, ".//sunset")))
-  sunrise_sunset_hhmm <- c(sunrise_time, sunset_time)
-  names(sunrise_sunset_hhmm) <- c("sunrise", "sunset")
-  return(sunrise_sunset_hhmm)
+#' get_sunrise_sunset(2017, 1, 13, 35.774781, 139.606076, "Asia/Tokyo")
+#' get_sunrise_sunset(2010, 3, 31, 25.1 ,138.4, "UTC")
+get_sunrise_sunset <- function(year, month, day, lat, lng, tz){
+  date <- as.Date(paste(year, month, day, sep="-"))
+  res <- getSunlightTimes(date, lat, lng, keep = c("sunrise", "sunset"), tz=tz)
+  sunrise_time <- as.POSIXlt(res[["sunrise"]])
+  sunset_time <- as.POSIXlt(res[["sunset"]])
+  print(sunrise_time)
+  print(sunset_time)
+  # print(str(sunrise_time))
+  # print(str(sunset_time))
+  sunrise_sunset_posixlt <- c(sunrise_time, sunset_time)
+  names(sunrise_sunset_posixlt) <- c("sunrise", "sunset")
+  return(sunrise_sunset_posixlt)
 }
 #
--- R/set_attr_day_night.R
+++ R/set_attr_day_night.R
@@ -4,6 +4,7 @@
 #' @param datetime_col_name 日時情報が格納された列名
 #' @param lat 緯度
 #' @param lng 経度
+#' @param tz タイムゾーン
 #' @param datetime_format 時間の列名のフォーマット
 #' @param sr_befor 日の出の何分前までをsunsetとするか
 #' @param sr_after 日の出の何分後までをsunsetとするか　
@@ -12,13 +13,13 @@
 #' @return DayNightという列名にDay/Night/Sunset/Sunriseのカテゴリを付与したデータフレーム
 #' @examples
 #' set_attr_day_night(df1, Datetime, 45.1 ,135.4)
-#' set_attr_day_night(df1, Datetime, 45.1 ,135.4, "%Y/%m/%d %H:%M", 0, 30, 15, 15)
-set_attr_day_night <- function(df, datetime_col_name, lat, lng, datetime_format="%Y-%m-%d %H:%M:%S", sr_befor=15, sr_after=15, ss_before=15, ss_after=15){
+#' set_attr_day_night(df1, Datetime, 45.1 ,135.4, "Asia/Tokyo", "%Y/%m/%d %H:%M", 0, 30, 15, 15)
+set_attr_day_night <- function(df, datetime_col_name, lat, lng, tz, datetime_format="%Y-%m-%d %H:%M:%S", sr_befor=15, sr_after=15, ss_before=15, ss_after=15){
   # 型チェック
   # 新たな列追加
   df[["DayNight"]] <- ""
   # フォーマットに従って時間型に変換
-  df[[datetime_col_name]] <- as.POSIXlt(df[[datetime_col_name]], tz="UTC", format=datetime_format)
+  df[[datetime_col_name]] <- as.POSIXlt(df[[datetime_col_name]], tz=tz, format=datetime_format)
   # 期間を抜き出す
   start_day <- as.Date(min(df[[datetime_col_name]]))
   end_day <- as.Date(max(df[[datetime_col_name]]))
@@ -31,7 +32,7 @@ set_attr_day_night <- function(df, datetime_col_name, lat, lng, datetime_format=
     # 日の出日の入り取得
     tmp_datetime <- as.POSIXlt(start_day + i)
     # print(tmp_datetime)
-    sunrise_sunset <- get_sunrise_sunset(tmp_datetime$year+1900, tmp_datetime$mon+1, tmp_datetime$mday, lat, lng)
+    sunrise_sunset <- get_sunrise_sunset(tmp_datetime$year+1900, tmp_datetime$mon+1, tmp_datetime$mday, lat, lng, tz)
     sunrise <- sunrise_sunset["sunrise"]
     sunset <- sunrise_sunset["sunset"]
     # print(sunrise)
